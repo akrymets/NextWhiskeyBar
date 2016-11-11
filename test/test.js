74a7b3a641425e604019f0fd26212957c22d245e
Andrii Krymets <andrii.krymets@finstar.com>
2016-11-11 14:19:39 +0200

    Add Test#3

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
 var assert = require('assert');
+var dbops = require('../modules/dbops');
+var mongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/nwb';
 
 describe('My test test', function() {
 	describe('Test #1', function(){
@@ -6,6 +10,21 @@ describe('My test test', function() {
 			assert.equal(-1, [1,2,3].indexOf(4));
 		});
 	});
+
+	describe('Test #3', function(){
+		it('Will return 0 when db is accessible', function(){
+			mongoClient.connect(url, function(err, db){
+				assert.equal(err, null);
+				console.log('Test connection to MongoDB server is successful');
+				dbops.findDocuments(db, 'places', function(docs){
+					it('Will fail if query result is empty', function(){
+						assert.equal(true, docs);
+					});
+				});
+				db.close();
+			});
+		});
+	});	
 
 	// describe('Test #2', function(){
 	// 	it('Will return -1 when the value is not present', function(){
