ab3aacf9e37ca1b487682938aa79b7b71160d061
Andrii Krymets <andrii.krymets@finstar.com>
2016-11-11 11:02:12 +0200

    Fix tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 
-// describe('My test test', function() {
+describe('My test test', function() {
 // 	describe('Test #1', function(){
 // 		it('Will return -1 when the value is not present', function(){
 // 			assert.equal(-1, [1,2,3].indexOf(4));
