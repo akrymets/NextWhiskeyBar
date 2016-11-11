e4923d1815683b1f3dd8df96051be1756cd1e9f6
Andrii Krymets <andrii.krymets@finstar.com>
2016-11-11 11:05:25 +0200

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,15 @@
 var assert = require('assert');
 
 describe('My test test', function() {
-// 	describe('Test #1', function(){
-// 		it('Will return -1 when the value is not present', function(){
-// 			assert.equal(-1, [1,2,3].indexOf(4));
-// 		});
-// 	});
-
-	describe('Test #2', function(){
+	describe('Test #1', function(){
 		it('Will return -1 when the value is not present', function(){
-			assert.equal(-1, [1,2,3].indexOf(1));
+			assert.equal(-1, [1,2,3].indexOf(4));
 		});
 	});
+
+	// describe('Test #2', function(){
+	// 	it('Will return -1 when the value is not present', function(){
+	// 		assert.equal(-1, [1,2,3].indexOf(1));
+	// 	});
+	// });
 });
